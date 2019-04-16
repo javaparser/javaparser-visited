aa1b6c68fff23122da7ef7493db57f44f58da64a
Nicholas Smith <nicholas.p.smith@live.co.uk>
2019-04-16 08:38:16 +0100

    Fix file path

--- src/main/java/org/javaparser/examples/chapter2/CommentReporterStarter.java
+++ src/main/java/org/javaparser/examples/chapter2/CommentReporterStarter.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class CommentReporterStarter {
 
-    private static final String FILE_PATH = "src/main/java/org/javaparser/examples/samples/ReversePolishNotation.java";
+    private static final String FILE_PATH = "src/main/java/org/javaparser/samples/ReversePolishNotation.java";
 
     public static void main(String[] args) throws Exception {
 
